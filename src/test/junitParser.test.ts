6a891db95c01ac3e8d9eb77bbe33e897486a219a
ETtestim <e.tal@tricentis.com>
2024-07-08 10:51:56 +0300

    remove old tests

--- src/test/junitParser.test.ts
+++ src/test/junitParser.test.ts
@@ -3,18 +3,6 @@ import { parseTestReports } from '../junitParser';
 
 const DIRNAME = __dirname;
 
-// describe('JunitParser', () => {
-//     it('should parse junit report with 3 suites', async () => {
-//         const testReports = await parseTestReports('test', 'test', `${DIRNAME}/xml/testsuites.xml`, []);
-//         assert.strictEqual(testReports.length, 3);
-//         assert.deepEqual(testReports[0], getFirstSuite('xml/testsuites.xml'));
-//     });
-//     it('should parse junit report with 1 suite', async () => {
-//         const testReports = await parseTestReports('test', 'test', `${DIRNAME}/xml/testsuite.xml`, []);
-//         assert.strictEqual(testReports.length, 1);
-//         assert.deepEqual(testReports[0], getFirstSuite('xml/testsuite.xml'));
-//     });
-// });
 test('should parse junit report with 3 suites', async (t: any) => {
     const testReports = await parseTestReports('test', 'test', `${DIRNAME}/xml/testsuites.xml`, []);
     t.assert.snapshot(testReports.length);
